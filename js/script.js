2ed84cde505380f391f315d86222cd7621780961
DaniLott1988 <danilott1988@gmail.com>
2021-09-24 14:33:54 +0200

    Update script.js
    Part 1 and 2 jQuery of 4

--- js/script.js
+++ js/script.js
@@ -2,24 +2,26 @@ function newItem(){
 
 //javascript
 //1. Adding a new item to the list of items:
-   let li = document.createElement("li");
-   let inputValue = document.getElementById("input").value;
+   let list = $('#list');
+   let li = $('<li></li>')
+   let inputValue = $('input').value();
    let text = document.createTextNode(inputValue);
-   li.appendChild(text);
+   li.append(inputValue);
 
    if (inputValue === '') {
      alert("You must write something!");
    } else {
-     let list = document.querySelector('#list');
-     list.appendChild(li);
+     $('#list').appendChild(li);
    }
 
  //2. Crossing out an item from the list of items:
    function crossOut() {
- 		li.classList.toggle("strike");
+ 		li.toggleClass("strike");
  	}
 
- 	li.addEventListener("dblclick",crossOut);
+ 	li.on("dblclick", function crossOut(){
+    li.toggleClass("strike");
+  });
 
  //3(i). Adding the delete button "X":
    let crossOutButton = document.createElement("crossOutButton");
